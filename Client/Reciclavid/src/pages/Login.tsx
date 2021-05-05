818252971f7e7494abaaf857521de6e59f0ee882
A <201318652@ingenieria.usac.edu.gt>
2021-05-05 01:21:02 -0600

    Login y registro

---
+++ Client/Reciclavid/src/pages/Login.tsx
@@ -0,0 +1,129 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import axios from "axios";
+import { IonGrid, IonRow, IonCol } from '@ionic/react';
+import { personCircle } from "ionicons/icons";
+import { useHistory } from "react-router-dom";
+import { IonItem, IonLabel, IonInput, IonButton, IonIcon, IonAlert } from '@ionic/react';
+
+function validateUsername(username: string) {
+    const re = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/;
+    return re.test(String(username).toLowerCase());
+}
+const Login: React.FC = () => {
+  const history = useHistory();
+  const [username, setusername] = useState<string>("eve.holt@reqres.in");
+  const [password, setPassword] = useState<string>("cityslicka");
+  const [iserror, setIserror] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+  const handleLogin = () => {
+    if (!username) {
+        setMessage("¡Tu usuario esta incorrecto!");
+        setIserror(true);
+        return;
+    }
+    if (validateUsername(username) === false) {
+        setMessage("¡Usuario invalido!");
+        setIserror(true);
+        return;
+    }
+
+    if (!password || password.length < 6) {
+        setMessage("Por favor ingresa tu contraseña");
+        setIserror(true);
+        return;
+    }
+
+    const loginData = {
+        "username": username,
+        "password": password
+    }
+
+    const api = axios.create({
+        baseURL: `https://reqres.in/api`
+    })
+    api.post("/login", loginData)
+        .then(res => {             
+            history.push("/dashboard/" + username);
+         })
+         .catch(error=>{
+            setMessage("Auth failure! Please create an account");
+            setIserror(true)
+         })
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Login</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="ion-padding ion-text-center">
+        <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonAlert
+                isOpen={iserror}
+                onDidDismiss={() => setIserror(false)}
+                cssClass="my-custom-class"
+                header={"Error!"}
+                message={message}
+                buttons={["Dismiss"]}
+            />
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonIcon
+                style={{ fontSize: "70px", color: "#0040ff" }}
+                icon={personCircle}
+            />
+          </IonCol>
+        </IonRow>
+          <IonRow>
+            <IonCol>
+            <IonItem>
+            <IonLabel position="floating"> Username </IonLabel>
+            <IonInput
+                type="email"
+                value={username}
+                onIonChange={(e) => setusername(e.detail.value!)}
+                >
+            </IonInput>
+            </IonItem>
+            </IonCol>
+          </IonRow>
+
+          <IonRow>
+            <IonCol>
+            <IonItem>
+              <IonLabel position="floating"> Contraseña </IonLabel>
+              <IonInput
+                type="password"
+                value={password}
+                onIonChange={(e) => setPassword(e.detail.value!)}
+                >
+              </IonInput>
+            </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <p style={{ fontSize: "small" }}>
+                  Al presionar "Iniciar sesión" aceptas nuestra <a href="#">Politica de privacidad</a>
+              </p>
+              <IonButton expand="block" onClick={handleLogin}>Iniciar sesión</IonButton>
+              <p style={{ fontSize: "medium" }}>
+                  ¿No tienes cuenta? <a href="registro">¡Registrate!</a>
+              </p>
+
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Login;
---
+++ Client/Reciclavid/src/pages/Registro.tsx
@@ -0,0 +1,142 @@
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import React, { useState } from 'react';
+import axios from "axios";
+import { IonGrid, IonRow, IonCol } from '@ionic/react';
+import { personCircle } from "ionicons/icons";
+import { useHistory } from "react-router-dom";
+import { IonItem, IonLabel, IonInput, IonButton, IonIcon, IonAlert } from '@ionic/react';
+
+function validateEmail(email: string) {
+    const re = /^((?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\]))$/;
+    return re.test(String(email).toLowerCase());
+}
+const Registro: React.FC = () => {
+  const history = useHistory();
+  const [email, setEmail] = useState<string>("eve.holt@reqres.in");
+  const [username, setUsername] = useState<string>("Tepo");
+  const [password, setPassword] = useState<string>("pistol");
+  const [iserror, setIserror] = useState<boolean>(false);
+  const [message, setMessage] = useState<string>("");
+  const handleRegistro = () => {
+    if (!email) {
+        setMessage("¡Correo invalido!");
+        setIserror(true);
+        return;
+    }
+    if (validateEmail(email) === false) {
+        setMessage("¡Tu correo esta incorrecto!");
+        setIserror(true);
+        return;
+    }
+
+    if (password.length < 6) {
+        setMessage("Ingresa una contraseña válida");
+        setIserror(true);
+        return;
+    }
+
+    const registroData = {
+        "email": email,
+        "password": password
+    }
+
+    const api = axios.create({
+        baseURL: `https://reqres.in/api`
+    })
+    api.post("/register", registroData)
+        .then(res => {             
+            history.push("/dashboard/" + email);
+         })
+         .catch(error=>{
+            setMessage("¡Error! Por favor crea una cuenta.");
+            setIserror(true)
+         })
+  };
+
+  return (
+    <IonPage>
+      <IonHeader>
+        <IonToolbar>
+          <IonTitle>Registro</IonTitle>
+        </IonToolbar>
+      </IonHeader>
+      <IonContent fullscreen className="ion-padding ion-text-center">
+        <IonGrid>
+        <IonRow>
+          <IonCol>
+            <IonAlert
+                isOpen={iserror}
+                onDidDismiss={() => setIserror(false)}
+                cssClass="my-custom-class"
+                header={"Error!"}
+                message={message}
+                buttons={["Dismiss"]}
+            />
+          </IonCol>
+        </IonRow>
+        <IonRow>
+          <IonCol>
+            <IonIcon
+                style={{ fontSize: "70px", color: "#0040ff" }}
+                icon={personCircle}
+            />
+          </IonCol>
+        </IonRow>
+          <IonRow>
+            <IonCol>
+            <IonItem>
+            <IonLabel position="floating">Email</IonLabel>
+            <IonInput
+                type="email"
+                value={email}
+                onIonChange={(e) => setEmail(e.detail.value!)}
+                >
+            </IonInput>
+            </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+            <IonItem>
+            <IonLabel position="floating"> Usuario</IonLabel>
+            <IonInput
+                type="text"
+                value={username}
+                onIonChange={(e) => setEmail(e.detail.value!)}
+                >
+            </IonInput>
+            </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+            <IonItem>
+              <IonLabel position="floating"> Password</IonLabel>
+              <IonInput
+                type="password"
+                value={password}
+                onIonChange={(e) => setPassword(e.detail.value!)}
+                >
+              </IonInput>
+            </IonItem>
+            </IonCol>
+          </IonRow>
+          <IonRow>
+            <IonCol>
+              <p style={{ fontSize: "small" }}>
+              Al presionar "Iniciar sesión" aceptas nuestra <a href="#">Politica de privacidad</a>
+              </p>
+              <IonButton expand="block" onClick={handleRegistro}>Registrarme</IonButton>
+              <p style={{ fontSize: "medium" }}>
+                  ¿Ya tienes una cuenta? <a href="registro">¡Inicia sesión!</a>
+              </p>
+
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
+    </IonPage>
+  );
+};
+
+export default Registro;
